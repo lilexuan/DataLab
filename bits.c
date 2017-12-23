77190825b0597028fcfaa8974685ea249510c805
kelvin <834524281@qq.com>
2017-12-23 11:21:09 +0800

    finish 1.bitAnd 2.getByte

--- bits.c
+++ bits.c
@@ -139,7 +139,7 @@ NOTES:
  *   Rating: 1
  */
 int bitAnd(int x, int y) {
-  return 2;
+	return ~((~x)|(~y));
 }
 /* 
  * getByte - Extract byte n from word x
@@ -150,14 +150,8 @@ int bitAnd(int x, int y) {
  *   Rating: 2
  */
 int getByte(int x, int n) {
-
-
-
-
-
-
-
-  return 2;
+	int result = x >> ((n) << 3);
+	return result & 0xff;
 
 }
 /* 
